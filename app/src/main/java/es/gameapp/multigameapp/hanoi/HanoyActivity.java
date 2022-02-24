bd9f4cc384b654a4f72d03a70fba5760fad65faa
Aintzane <aintzane.caamanofa@elorrieta-errekamari.com>
2022-02-24 20:38:44 +0100

    other activities

---
+++ app/src/main/java/es/gameapp/multigameapp/hanoi/HanoyActivity.java
@@ -0,0 +1,14 @@
+package es.gameapp.multigameapp.hanoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import es.gameapp.multigameapp.R;
+public class HanoyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hanoy);
+    }
+}
---
+++ app/src/main/java/es/gameapp/multigameapp/sopa/SopaActivity.java
@@ -0,0 +1,17 @@
+package es.gameapp.multigameapp.sopa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.gameapp.multigameapp.R;
+
+
+public class SopaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sopa);
+    }
+}
