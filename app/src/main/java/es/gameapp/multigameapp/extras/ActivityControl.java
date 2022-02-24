9be8c5355dec1a3b3ec97787c97fb15b17fbf5c2
JonatanElorrieta <60236918+JonatanElorrieta@users.noreply.github.com>
2022-02-24 20:59:24 +0100

    extras añadidos

---
+++ app/src/main/java/es/gameapp/multigameapp/extras/ActivityControl.java
@@ -0,0 +1,13 @@
+package es.gameapp.multigameapp.extras;
+
+public class ActivityControl {
+
+    public static final int MAIN_ACTIVITY_1=1;
+    public static final int REGISTER_ACTIVITY_2=2;
+    public static final int BASE_ACTIVITY_3=3;
+    public static final int GUARDAR_ACTIVITY_4=4;
+    public static final int LIST_ACTIVITY_5=5;
+    public static final int DETAIL_ACTIVITY_6=6;
+    public static final int CAMBIAR_PASS_ACTIVITY_7=7;
+
+}
